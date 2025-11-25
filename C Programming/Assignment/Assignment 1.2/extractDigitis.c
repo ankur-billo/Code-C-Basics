cd936357ef905d2b84bbe9db3fe5e71690ab2ce0
Ankur Sardar <ankursardar.work@gmail.com>
2025-11-25 10:03:45 +0530

    finally

--- C Programming/Assignment/Assignment 1.2/extractDigitis.c
+++ C Programming/Assignment/Assignment 1.2/extractDigitis.c
@@ -1,34 +1,50 @@
 #include <stdio.h>
 int main()
 {
-    int a = 59841;
-    //right to left
+    int n,org;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+    org=n;
 
-        while (a > 0)
+    //right to left
+    printf("Right to Left: \n");
+        while (n > 0)
         {
-            int dig = a % 10;
-            printf("%d \n", dig);
-            a = a / 10;
+            int dig = n % 10;
+            printf("%d\n", dig);
+            n = n / 10;
         }
         
-//left to right
-
-        int div = 1;
-        int temp = a;
-        while (temp >= 10)
-        {
-            temp = temp / 10;
-            div = div * 10;
-            // printf("%d ",temp);
+        //left to right
+        printf("Left to Right: \n");
+        int copy=org;
+        int div=1;
+        while(copy>=10){
+            copy=copy/10;
+            div=div*10;
         }
-        while (div > 0)
-        {
-            int digi = a / div;
-            printf("%d \n", digi);
-            a = a % div;
-            div = div / 10;
+        n=org;
+
+        while(div>0){
+            int digit=n/div;
+            printf("%d\n",digit);
+            n=n%div;
+            div=div/10;
         }
-
-
+        
+        //left to right
+                // int temp=a;
+                // int rev=0;
+                // while(temp>0){
+                //     int dig=temp%10; // last digit extraction
+                //     rev=rev*10+dig; // adding the dig to create the reversed number
+                //     temp=temp/10; // remove the last digit
+                // }
+                // while (rev > 0)
+                // {
+                //     int digi = rev % 10;
+                //     printf("%d \n", digi);
+                //     rev = rev / 10;
+                // }
     return 0;
 }
