0868952f476b34d6ead04ea80a9320276eb89406
Ankur Sardar <ankursardar.work@gmail.com>
2025-12-20 18:16:31 +0530

    a new logic

--- C Programming/WTH C/Array/reverseWithoutExtrs.c
+++ C Programming/WTH C/Array/reverseWithoutExtrs.c
@@ -1,1 +1,23 @@
 // reverese the array without using any extra array
+
+#include<stdio.h>
+#define SIZE 7
+int main(){
+    int arr[SIZE];
+    printf("Enter the elements of the array : \n");
+    for (int i = 0; i < SIZE; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int temp=0;
+    for(int i=0,j=SIZE-1;i<j;i++,j--){
+        temp =arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+     for (int i = 0; i < SIZE; i++)
+    {
+        printf("%d  ",arr[i]);
+    }
+    return 0;
+}
